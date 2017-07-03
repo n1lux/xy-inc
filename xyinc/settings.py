cc31c49986b1544eaf5ce1259a88cff23fd3b391
n1lux <n1lux.comp@gmail.com>
2017-07-03 17:59:21 -0300

    Remove test-without-migrate

--- xyinc/settings.py
+++ xyinc/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'test_without_migrations',
     'xyinc.api',
     'xyinc.utils',
 ]
